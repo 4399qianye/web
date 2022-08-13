fe53ccb2d347f752b95e8d3ecde9efe5ee41b3d2
4399qianye <101877376+4399qianye@users.noreply.github.com>
2022-08-13 23:51:13 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -93,15 +93,13 @@ fetch('https://v1.hitokoto.cn?max_length=24')
 //获取天气
 //每日限量 100 次
 //请前往 https://www.tianqiapi.com/ 申请（免费）
-fetch('https://www.yiketianqi.com/free/day?appid=43986679&appsecret=TksqGZT7&unescape=1')
+fetch('https://yiketianqi.com/api?unescape=1&version=v6&appid=43986679&appsecret=TksqGZT7')
     .then(response => response.json())
     .then(data => {
-        $('#wea_text').html(data.wea)
-        $('#city_text').html(data.city)
-        $('#tem_night').html(data.tem_night)
-        $('#tem_day').html(data.tem_day)
-        // $('#win_text').html(data.win)
-        // $('#win_speed').html(data.win_speed)
+        //$('#wea_text').html(data.wea + '&nbsp;' + data.tem_night + '℃' + '&nbsp;~&nbsp;' + data.tem_day + '℃')
+        $('#wea_text').text(data.wea)
+        $('#tem1').text(data.tem1)
+        $('#tem2').text(data.tem2)
     })
     .catch(console.error)
 
